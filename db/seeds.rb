180381167eeae9ed035b7689a70080a22164903d
magnito2 <magnusotwani@gmail.com>
2020-05-11 09:42:40 +0300

    seeding schools, needed for start

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
-Role.create_default_roles("greenlight")
-Rake::Task['admin:create'].invoke
+# Role.create_default_roles("greenlight")
+# Rake::Task['admin:create'].invoke
+
+school_list = [
+  "Othaya Girls",
+  "Default"
+]
+
+school_list.each do |school|
+  School.create(name: school) if School.find_by_name(school).blank?
+end
